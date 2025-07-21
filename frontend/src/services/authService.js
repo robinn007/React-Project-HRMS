98119f6256785196110cec169492bb930a28d87d
robinn007 <robinsinghc87@gmail.com>
2025-07-21 13:53:42 +0530

    backend url added

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.API_URL || "http://localhost:5000/api",
+  baseURL:  "https://react-project-hrms.onrender.com/api",
   headers: { "Content-Type": "application/json" },
 });
 
--- frontend/src/services/candidateService.js
+++ frontend/src/services/candidateService.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.API_URL || 'http://localhost:5000/api',
+  baseURL: import.meta.env.API_URL || 'https://react-project-hrms.onrender.com/api',
   headers: { 'Content-Type': 'application/json' },
 });
 
